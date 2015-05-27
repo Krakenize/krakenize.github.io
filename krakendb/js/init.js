f5200fcac74cab2b3915781bf31018cbb9a363fd
Chyngyz Dyikanbaev <chipalino@live.com>
2015-05-27 15:05:58 -0400

    kraken

---
+++ krakendb/js/init.js
@@ -0,0 +1,9 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    $('.dropdown-button').dropdown({hover: false});
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
